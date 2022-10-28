87d5b5d79f6e9b5dc15e09eecddafddf0037790a
Yuefeng Wang <93065511+euphw@users.noreply.github.com>
2022-10-27 21:47:18 -0400

    adding and deleting database and rendering items

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date=require(__dirname + "/date.js")
+//const date=require(__dirname + "/date.js")
 
 const app = express();
 
@@ -15,28 +15,81 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
 
+const itemSchema = {
+  name:String
+}
+
+const Item = mongoose.model("Item",itemSchema);
+
+const item1 = new Item({
+  name:"Welcome to your todolist."
+})
+
+const item2 = new Item({
+  name:"Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+  name:"<-- Hit this to delete an item."
+})
+
+const defaultItems=[item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("success!");
+  }
+});
 
 app.get("/",function(req,res){
-  const day = date.getDate();
-
-  res.render("list",{listTitle: day, newListItems: items});
-
+  //const day = date.getDate();
+  Item.find({},function(err,foundItems){
+    if(foundItems.length ===0){
+      Item.insertMany(defaultItems,function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("success!");
+        }
+      });
+      res.redirect("/");
+    }else{
+      res.render("list",{listTitle: "Today", newListItems: foundItems});
+    }
+  });
 })
 
 app.post("/",function(req, res){
-  const item=req.body.newItem;
-  if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect("/work");
-  }else{
-    items.push(item);
-    res.redirect("/");
-  }
+  
+  const itemName=req.body.newItem;
+  const item = new Item({
+    name:itemName
+  });
+  item.save();
+  res.redirect("/");
+  // if(req.body.list === "Work"){
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // }else{
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
+});
+
+app.post("/delete",function(req, res){
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId,function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/work",function(req,res){
   res.render("list",{listTitle: "Work List", newListItems: workItems});
-})
+});
 
 app.get("/about",function(req,res){
   res.render("about");
